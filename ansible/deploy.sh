d7b00976815ced4b36074b63ad9e668b048b3e91
Alfredo Sanchez <alfre32@gmail.com>
2021-06-25 00:44:20 +0200

    modified ansible files

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # Automatizando el despliegue de kubernetes (Ansible)
 
-# Tareas previas de configuración (master.local y worker.local)
+# Tareas previas de configuración (master y worker)
 ansible-playbook -i hosts -l master:workers 01-initial-conf.yaml
 
-# Instalación del servidor NFS (nfs.local) 02-install-nfs.yaml
+# Instalación del servidor NFS (nfs) 02-install-nfs.yaml
 ansible-playbook -i hosts -l nfs 02-install-nfs.yaml
 
-# Tareas comunes a realizar en el nodo master y los workers (master.local y worker.local)
+# Tareas comunes a realizar en el nodo master y los workers (master y worker)
 ansible-playbook -i hosts -l master:workers 03-common-tasks.yaml
 
-# Configurando kubernetes en el nodo master (master.local)
+# Configurando kubernetes en el nodo master (master)
 ansible-playbook -i hosts -l master 04-config-kubernetes.yaml
 
-# Instalando la SDN (master.local)
+# Instalando la SDN (master)
 ansible-playbook -i hosts -l master 05-install-sdn.yaml
 
-# Configurando los workers (worker.local)
+# Configurando los workers (worker)
 ansible-playbook -i hosts -l master:workers 06-config-workers.yaml
 
-# Instalando un ingress controller (master.local)
+# Instalando un ingress controller (master)
 ansible-playbook -i hosts -l master 07-install-ingress-controler.yaml
 
-# Creamos un usuario no administrador para la gestión del clúster (master.local)
+# Creamos un usuario no administrador para la gestión del clúster (master)
 ansible-playbook -i hosts -l master 08-create-no-root-user.yaml
