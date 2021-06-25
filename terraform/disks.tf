67c1ac28d2f884ba4b4d805d17243c3bc601ab93
Alfredo Sanchez <alfre32@gmail.com>
2021-06-25 12:38:24 +0200

    modified terraform files

--- terraform/disks.tf
+++ terraform/disks.tf
@@ -8,7 +8,7 @@ resource "azurerm_managed_disk" "myManagedDisk" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "myDataDisk" {
-    count             = "${var.vms_disks == 1 ? 1 : 0}"
+    count             = "${var.vms == "nfs" ? 1 : 0}"
     managed_disk_id    = azurerm_managed_disk.myManagedDisk.id
     virtual_machine_id = azurerm_virtual_machine.myVM[count.index].id
     lun                = "10"
