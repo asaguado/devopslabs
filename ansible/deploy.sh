7b02bdbecb22befcce5c54b6e534050d83262e07
Alfredo Sanchez <alfre32@gmail.com>
2021-06-29 16:07:24 +0200

    ansible: fix msg become_method: sudo

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,13 +2,13 @@
 # Automatizando el despliegue de kubernetes (Ansible)
 
 # Tareas previas de configuración (master y worker)
-#ansible-playbook -i hosts -l master:workers 01-initial-conf.yaml
+ansible-playbook -i hosts -l master:workers 01-initial-conf.yaml
 
 # Instalación del servidor NFS (nfs) 02-install-nfs.yaml
 #ansible-playbook -i hosts -l nfs 02-install-nfs.yaml
 
 # Tareas comunes a realizar en el nodo master y los workers (master y worker)
-ansible-playbook -i hosts -l master:workers 03-common-tasks.yaml
+#ansible-playbook -i hosts -l master:workers 03-common-tasks.yaml
 
 # Configurando kubernetes en el nodo master (master)
 #ansible-playbook -i hosts -l master 04-config-kubernetes.yaml
